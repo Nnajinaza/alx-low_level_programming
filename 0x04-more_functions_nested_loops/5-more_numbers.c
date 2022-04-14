50ff352aae7d4c5d2db51856cc65b91cf31c5daf
Nnajinaza <nnajinaza@gmail.com>
2022-04-14 17:53:25 +0000

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int a = 0;
+
+	while (a <= 10)
+	{
+		int b = 0;
+
+		while (b <= 9)
+		{
+			_putchar(b % 10 + '0');
+			b++;
+		}
+		while (b > 9 && b <= 14)
+		{
+		  	_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			b++;
+		}
+	_putchar('\n');
+	a++;
+	}
+}
