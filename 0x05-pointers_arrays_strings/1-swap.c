e231218ac6c2f43cfdb711d08447b6d417dfd21d
Nnajinaza <nnajinaza@gmail.com>
2022-04-21 14:37:04 +0000

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - Entry point
+ * @a: value of integer
+ * @b: value of integer
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*b = *a;
+	*a = *a;
+}
