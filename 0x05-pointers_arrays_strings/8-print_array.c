5c75bd178b93f472dfe1561c5b694d7488a8bebd
Nnajinaza <nnajinaza@gmail.com>
2022-04-21 22:54:06 +0000

    pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <stdio.h>
- /* print_array - prints n elements of an array of integers
-  *@a: A pointer to an int that will be updated/ changed
-  *@n: return value n
- **Return: void which ,means our answer is correct
-  *      */
 
- void print_array(int *a, int n)
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: A pointer to an int that will be updated/ changed
+ * @n: return value n
+ * Return: void which ,means our answer is correct
+ */
 
+void print_array(int *a, int n)
 {
 	int i;
 
@@ -24,7 +25,6 @@
 		i++;
 	}
 
-	printf("\n")
-		;
+	printf("\n");
 }
 
