687f9167886a1163ec0e862a668e716c61bcdf01
Nnajinaza <nnajinaza@gmail.com>
2022-04-25 09:36:57 +0000

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,19 +13,19 @@ int main(void)
 	int num3;
 	int num4;
 
-	for (num1 = '0'; num1 <= '9'; num1++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = '0'; num2 <= '8'; num2++)
+		for (num2 = 0; num2 <= 8; num2++)
 		{
-			for (num3 = num1; num3 <= '9'; num3++)
+			for (num3 = num1; num3 <= 9; num3++)
 			{
-				for (num4 = (num2 + '1'); num4 <= '9'; num4++)
+				for (num4 = (num2 + 1); num4 <= 9; num4++)
 				{
-					putchar('0' + num1);
-					putchar('0' + num2);
+					putchar(num1 + '0');
+					putchar(num2 + '0');
 					putchar(' ');
-					putchar('0' + num3);
-					putchar('0' + num4);
+					putchar(num3 + '0');
+					putchar(num4 + '0');
 
 					if (num1 <= 9 && num2 <= 8)
 					{
