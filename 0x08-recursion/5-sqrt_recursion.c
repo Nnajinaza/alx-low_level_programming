e6ca34d5fe8c756e5c2c2cc13e0f6d6972bb4a66
Nnajinaza <nnajinaza@gmail.com>
2022-04-28 22:54:11 +0000

    Task prime

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,36 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Entry point
- * @n: natural nos value
- * Return: Always 0
+ *_evaluate - Entry point
+ *@i: interger
+ *@n: interger
+ *Return: evaluate sqrt
+ */
+
+int _evaluate(int i, int n)
+{
+
+	if (n == 0 || n == 1)
+		return (n);
+
+	else if (i * i < n)
+		return (_evaluate(i + 1, n));
+
+	else 
+	return (i);
+}
+
+/**
+ *_sqrt_recursion - Entry point
+ *@n: interger
+ *Return: Always 0
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n <= 0)
-		return -1;
-	else
-		return n * _sqrt_recursion(n / 2);
+	int i = 0;
+
+	if (i < 0) 
+		return (-1);
+	return (_evaluate(i, n));
 }
