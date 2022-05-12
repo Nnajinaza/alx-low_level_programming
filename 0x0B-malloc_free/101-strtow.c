942b079d6fc329bfae3e80c098fead706157bc53
Nnajinaza <nnajinaza@gmail.com>
2022-05-12 09:11:20 +0000

    New config

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -49,7 +49,6 @@ char **strtow(char *str)
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
 			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
-			j++;
 			w[wc] = (char *)malloc(j * sizeof(char));
 			j--;
 			if (w[wc] == NULL)
