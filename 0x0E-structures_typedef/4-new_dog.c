87e4cb5176525d42acd521a77a2b6a54faff64cf
Nnajinaza <nnajinaza@gmail.com>
2022-05-12 01:01:07 +0000

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,9 +11,7 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int a = 0;
-	unsigned int b = 0;
-	unsigned int c = 0;
+	unsigned int a, b, c;
 
 	dog_t *dog;
 
@@ -22,26 +20,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	while (name[a])
-	{
-		a++;
-	}
+	for (a = 0; name[a]; a++)
 	dog->name = malloc(a * sizeof(char));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	while (c < a)
+	for (c = 0; c < a; c++)
 	{
-		c++;
 		dog->name[c] = name[c];
 	}
 	dog->age = age;
-	while (owner[b])
-	{
-		b++;
-	}
+	for (b = 0; owner[b]; b++)
 	dog->owner = malloc(b * sizeof(char));
 	if (dog->owner == NULL)
 	{
@@ -49,10 +40,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-	while (c < b)
-	{
-		c++;
-	}
+	for (c = 0; c < b; c++)
 		dog->owner[c] = owner[c];
 	return (dog);
 }
