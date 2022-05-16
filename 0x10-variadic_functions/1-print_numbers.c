03d4918cbd8a9329750d294085f85daddf656c7f
Nnajinaza <nnajinaza@gmail.com>
2022-05-16 18:03:23 +0000

    print_number updated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		int j = va_arg(args, int);
-		printf{"%d", j);
+		printf("%d", j);
 		if (i < n && separator)
 		{
 			printf("%s", separator);
