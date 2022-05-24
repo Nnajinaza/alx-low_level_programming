c386a6b3e3469b7ac8bdec28b6c29874791d9f3f
Nnajinaza <nnajinaza@gmail.com>
2022-05-24 23:35:27 +0000

    Create 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * _length - function to get string length
+ * @str: array of char
+ *
+ * Return: length of array
+ */
+unsigned int _length(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * *add_node - function to add node at the beginning of a list
+ * @head: first node in the list
+ * @str: str to add in the list
+ *
+ * Return: address of the new element on success or NULL
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	if (str == NULL)
+		return (NULL);
+	new = (list_t *) malloc(sizeof(list_t));
+	new->str = strdup(str);
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	else
+	{
+		new->len = _length(new->str);
+		new->next = *head;
+		*head = new;
+	}
+	return (new);
+}
