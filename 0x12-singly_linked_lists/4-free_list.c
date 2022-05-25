03822b885773ed73660e8c12162195847d810bb7
Nnajinaza <nnajinaza@gmail.com>
2022-05-25 01:27:41 +0000

    Update free_list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,9 +10,13 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
+	if (head == NULL)
+		return;
+
 	while (head != NULL)
 	{
 		temp = head;
+		free(head->str);
 		head = head->next;
 		free(temp);
 	}
