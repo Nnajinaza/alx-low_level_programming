b6c5b2cc4eb464a1d4ef8a3d2e3c862c3131fd1d
Nnajinaza <nnajinaza@gmail.com>
2022-05-26 23:41:19 +0000

    5-free_listint2.c update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,10 +13,11 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (*head != NULL)
+	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
+		*head = NULL;
 	}
 }
