783111aa83c78b2af997ab7522b446439df17511
Nnajinaza <nnajinaza@gmail.com>
2022-05-26 23:55:06 +0000

    code fix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,11 +12,11 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 
-	temp = *head->next;
-	*head = temp;
+	temp = *head;
+	*head = temp->next;
 	n = temp->n;
 	free(temp);
 	return (n);
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp;
 	unsigned int count = 0;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	temp = head;
@@ -23,5 +23,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		count++;
 		temp = temp->next;
 	}
-	return (head);
+	return (temp);
 }
