ae78f314107c30fd38d1e842003c7bd9a2c26f93
Nnajinaza <nnajinaza@gmail.com>
2022-05-26 22:24:19 +0000

    Create 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,16 +12,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	if (head == NULL)
-		return (NULL);
-
 	new = (listint_t *) malloc(sizeof(listint_t));
 
-	if (new == NULL)
-	{
-		free(new);
+	if (new != NULL)
 		return (NULL);
-	}
+
 	new->n = n;
 	new->next = *head;
 	*head = new;
