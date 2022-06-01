7d6cff2e91328dfc2c4ee9b6c996cdbcd822188b
Nnajinaza <nnajinaza@gmail.com>
2022-06-01 23:14:33 +0000

    Create 2-append_text_to_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
 
 	if (fd == -1)
 		return (-1);
@@ -22,7 +22,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 
-		for (i = 0; text_content[i]; i++);
+		for (i = 0; text_content[i]; i++)
+			;
 		length = write(fd, text_content, i);
 		if (length == -1)
 			return (-1);
